0c98dd3104e75f961f810f1c87ed0259058146e9
zhangwei <2680501124@qq.com>
2024-04-24 17:11:06 +0800

    improve

--- jsls-core/src/main/java/com/jsls/util/WebUtils.java
+++ jsls-core/src/main/java/com/jsls/util/WebUtils.java
@@ -5,7 +5,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
 import java.net.URLDecoder;
+import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -154,6 +157,62 @@ public class WebUtils {
             // ignore
         }
         return fileName;
+    }
+
+    public static String getDomain() {
+        HttpServletRequest request = getRequest();
+        StringBuffer sb = request.getRequestURL();
+        return sb.delete(sb.length() - request.getRequestURI().length(), sb.length()).toString();
+    }
+
+    public static String getOrigin() {
+        HttpServletRequest request = getRequest();
+        return request.getHeader("Origin");
+    }
+
+    /**
+     * 获取IP地址
+     * <p>
+     * 使用Nginx等反向代理软件， 则不能通过request.getRemoteAddr()获取IP地址
+     * 如果使用了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP地址，X-Forwarded-For中第一个非unknown的有效IP字符串，则为真实IP地址
+     */
+    public static String getIpAddr() {
+        HttpServletRequest request = getRequest();
+        String ip = request.getHeader("x-forwarded-for");
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (StringUtils.isEmpty(ip) || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        try {
+            String[] localhost = new String[] { "127.0.0.1", "0:0:0:0:0:0:0:1" };
+            if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getRemoteAddr();
+                if (Arrays.asList(localhost).contains(ip)) {
+                    // 根据网卡取本机配置的IP
+                    ip = InetAddress.getLocalHost().getHostAddress();
+                }
+            }
+            String comma = ",";
+            if (ip.contains(comma)) {
+                ip = ip.split(",")[0];
+            }
+            if (localhost[0].equals(ip)) {
+                // 获取本机真正的ip地址
+                ip = InetAddress.getLocalHost().getHostAddress();
+            }
+        } catch (UnknownHostException e) {
+            logger.error("获取IP异常:" + e.getMessage(), e);
+            // ignore
+        }
+        return ip;
     }
 
     public static boolean isAjax(HttpServletRequest request) {
